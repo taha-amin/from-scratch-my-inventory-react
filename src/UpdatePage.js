331731ee946b4e7cd25c4861eba76d226f157296
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-13 10:58:53 -0700

    created updatepage.js which fetches and displays the appropriate item on load. the useeffect dependency array will account for changes in the URL id param. on submit, update the item. on load, will preload the form

---
+++ src/UpdatePage.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import { getRestaurantById, updateRestaurant } from './services/fetch-utils';
+import { useHistory, useParams } from 'react-router-dom';
+
+export default function UpdatePage() {
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [restaurantInTheForm, setRestaurantInTheForm] = useState({
+    name: '',
+    location: '',
+    score: 0,
+    parking: true,
+    review: '',
+  });
+
+  useEffect(() => {
+    async function load() {
+      const restaurant = await getRestaurantById(id);
+
+      setRestaurantInTheForm(restaurant);
+    }
+    load();
+  }, [id]);
+
+  async function handleUpdateSubmit(e) {
+    e.preventDefault();
+
+    await updateRestaurant(id, restaurantInTheForm);
+
+    push('/restaurants');
+  }
+
+  return (
+    <div className="create-page">
+      <form onSubmit={handleUpdateSubmit}>
+        Update this restaurant
+        <label>
+          Name
+          <input
+            required
+            value={restaurantInTheForm.name}
+            onChange={(e) =>
+              setRestaurantInTheForm({ ...restaurantInTheForm, name: e.target.value })
+            }
+            name="name"
+          />
+        </label>
+        <label>
+          Location
+          <input
+            required
+            value={restaurantInTheForm.location}
+            onChange={(e) =>
+              setRestaurantInTheForm({ ...restaurantInTheForm, location: e.target.value })
+            }
+            name="location"
+          />
+        </label>
+        <label>
+          Score
+          <input
+            required
+            value={restaurantInTheForm.score}
+            onChange={(e) =>
+              setRestaurantInTheForm({ ...restaurantInTheForm, score: e.target.value })
+            }
+            name="score"
+          />
+        </label>
+        <label>
+          Parking
+          <input
+            type="radio"
+            required
+            value={restaurantInTheForm.parking}
+            onChange={(e) =>
+              setRestaurantInTheForm({ ...restaurantInTheForm, review: e.target.value })
+            }
+            name="parking"
+          />
+        </label>
+        <label>
+          Review
+          <textarea
+            required
+            value={restaurantInTheForm.review}
+            onChange={(e) =>
+              setRestaurantInTheForm({ ...restaurantInTheForm, review: e.target.value })
+            }
+            name="review"
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
