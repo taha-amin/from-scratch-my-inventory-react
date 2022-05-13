83ed1a2d80f435cf70379e5ad944502cceb8ff12
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-13 09:47:14 -0700

    created restaurant.js which is what will show up on restaurant lists page

---
+++ src/Restaurant.js
@@ -0,0 +1,18 @@
+import { Link } from 'react';
+
+export default function Restaurant({
+  restaurant: { name, location, yelp_score, parking, review },
+}) {
+  return (
+    <Link to={`/restaurants/${id}`}>
+      <div className="restaurant">
+        <h3>Restaurant Name: {name}</h3>
+        <p>
+          Located in {location}, this restaurant has a score of {yelp_score} on yelp.
+        </p>
+        <p>Parking? {parking}</p>
+        <p>See what other people are saying about this place: {review}</p>
+      </div>
+    </Link>
+  );
+}
